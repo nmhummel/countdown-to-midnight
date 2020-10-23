262afa3de1bab9980b6ad43a5cc860676b40f4f1
nmhummel <natalie.hummel@gmail.com>
2020-10-23 18:09:09 -0500

    commit tree 35d6eb68a81f2b077422afb316bc74397f377888

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,24 @@
 #write your code here
 
-def countdown
+def countdown(number)
+    while number > 0
+        puts "#{number} SECOND(S)!"
+        number -= 1
+    end
+    if number == 0 
+        return "HAPPY NEW YEAR!"
+    end
+    
 end
+
+countdown(10)
+
+def countdown_with_sleep(time)
+    while time > 0
+        puts "#{time} SECOND(S)!"
+        sleep(1)
+        time -= 1
+    end
+        return "HAPPY NEW YEAR!"
+end
+
